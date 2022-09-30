323f596f386fa7805378295229017f229eba776d
Matija997 <matijamarinkovic@gmail.com>
2022-09-30 03:40:59 +0200

    Odradjena forama za log in i registraciju
    Samo HTML i CSS

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <title>Dobrodošli</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="form-box">
+            <div class="button-box">
+                <div id="btn"></div>
+                <button type="button" class="toggle-btn" onclick="login()">Prijava</button>
+                
+                <button type="button" class="toggle-btn" onclick="register()">Registracija</button>
+            </div>
+            <form id="logIn" class="user-pass">
+            <input type="text" class="user" placeholder="Korisnićko ime" required>
+            <input type="text" class="user" placeholder="Unesi lozinku" required>
+            <input type="checkbox" class="check"><span>Zapamti lozinku</span>
+            <button type="submit" class="submit-btn">Prijavi se</button>
+
+            </form>
+            <form id="regis" class="user-pass">
+            <input type="text" class="user" placeholder="Korisničko ime" required>
+            <input type="text" class="user" placeholder="Email" required>
+            <input type="text" class="user" placeholder="Unesi lozinku" required>
+            <input type="checkbox" class="check" required><span>Prihvatam uslove korišćenja</span>
+            
+            <button type="submit" class="submit-btn">Registruj se</button>
+
+            </form>
+
+        </div>
+        
+       
+    </div>
+    <script>
+        var x=document.getElementById("logIn");
+        var y=document.getElementById("regis");
+        var z=document.getElementById("btn");
+        function register(){
+            x.style.left="-400px";
+            y.style.left="50px";
+            z.style.left="110px";
+        }
+        function login(){
+            x.style.left="50px";
+            y.style.left="450px";
+            z.style.left="0px";
+        }
+
+
+    </script>
+
+</body>
+</html>
